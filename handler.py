06925e9f3395433fa723f757458f7b4687a4c099
Maratik <marat.naiv@gmail.com>
2017-03-27 14:41:35 +0300

    My recent changes

--- handler.py
+++ handler.py
@@ -7,9 +7,22 @@ import lxml.etree as ET
 from lxml import objectify
 import json
 
-PATH = 'data'   # chinese_texts
-DICK_PATH = 'dic/cedict_ts.u8'
+PATH = '/Users/marat/Documents/ZhRuParCo/ruzhparallel-materials/corpus materials (2)'   # chinese_texts
+DICK_PATH = '/Users/marat/Documents/ZhRuParCo/ruzhparallel-materials/cedict_ts (1).u8'
 DICK_CACHE = 'dic/cedict.dat'
+
+def unescape(text):
+    def fixup(m):
+        text = m.group(0)
+        # character reference
+        try:
+            if text[:3] == "&#x":
+                return chr(int(text[3:-1], 16))
+            else:
+                return chr(int(text[2:-1]))
+        except ValueError:
+            pass
+    return re.sub("&#?\w+;", fixup, text)
 
 class ZhXMLProcessor():
     def __init__(self, dict_path):
@@ -57,9 +70,174 @@ class ZhXMLProcessor():
         definition = ''
         return definition
 
-    def clean_definition(self, definition, key, transcr):
-        # TODO: clean up definition as  in kuz.py
-        return definition
+    def convert_pinyin(self, car):
+        # car = car.lower()
+        car = re.sub(r"a5", "a", car)
+        car = re.sub(r"e5", "e", car)
+        car = re.sub(r"i5", "i", car)
+        car = re.sub(r"o5", "o", car)
+        car = re.sub(r"u5", "u", car)
+        car = re.sub(r"u:", "ü", car)
+        car = re.sub(r"ü5", "ü", car)
+        car = re.sub(r"a1", "ā", car)
+        car = re.sub(r"A1", "Ā", car)
+        car = re.sub(r"a2", "á", car)
+        car = re.sub(r"A2", "Á", car)
+        car = re.sub(r"a3", "ǎ", car)
+        car = re.sub(r"A3", "Ǎ", car)
+        car = re.sub(r"a4", "à", car)
+        car = re.sub(r"A4", "À", car)
+        car = re.sub(r"e1", "ē", car)
+        car = re.sub(r"E1", "Ē", car)
+        car = re.sub(r"e2", "é", car)
+        car = re.sub(r"E2", "É", car)
+        car = re.sub(r"e3", "ě", car)
+        car = re.sub(r"E3", "Ě", car)
+        car = re.sub(r"e4", "è", car)
+        car = re.sub(r"E4", "È", car)
+        car = re.sub(r"i1", "ī", car)
+        car = re.sub(r"i2", "í", car)
+        car = re.sub(r"i3", "ǐ", car)
+        car = re.sub(r"i4", "ì", car)
+        car = re.sub(r"o1", "ō", car)
+        car = re.sub(r"O1", "Ō", car)
+        car = re.sub(r"o2", "ó", car)
+        car = re.sub(r"O2", "Ó", car)
+        car = re.sub(r"o3", "ǒ", car)
+        car = re.sub(r"O3", "Ǒ", car)
+        car = re.sub(r"o4", "ò", car)
+        car = re.sub(r"O4", "Ò", car)
+        car = re.sub(r"u1", "ū", car)
+        car = re.sub(r"u2", "ú", car)
+        car = re.sub(r"u3", "ǔ", car)
+        car = re.sub(r"u4", "ù", car)
+        car = re.sub(r"ü1", "ǖ", car)
+        car = re.sub(r"ü2", "ǘ", car)
+        car = re.sub(r"ü3", "ǚ", car)
+        car = re.sub(r"ü4", "ǜ", car)
+        car = re.sub(r"an1", "ān", car)
+        car = re.sub(r"An1", "Ān", car)
+        car = re.sub(r"an2", "án", car)
+        car = re.sub(r"An2", "Án", car)
+        car = re.sub(r"an3", "ǎn", car)
+        car = re.sub(r"An3", "Ǎn", car)
+        car = re.sub(r"an4", "àn", car)
+        car = re.sub(r"An4", "Àn", car)
+        car = re.sub(r"ang1", "āng", car)
+        car = re.sub(r"Ang1", "Āng", car)
+        car = re.sub(r"ang2", "áng", car)
+        car = re.sub(r"Ang2", "Áng", car)
+        car = re.sub(r"ang3", "ǎng", car)
+        car = re.sub(r"Ang3", "Ǎng", car)
+        car = re.sub(r"ang4", "àng", car)
+        car = re.sub(r"Ang4", "Àng", car)
+        car = re.sub(r"en1", "ēn", car)
+        car = re.sub(r"En1", "Ēn", car)
+        car = re.sub(r"en2", "én", car)
+        car = re.sub(r"En2", "Én", car)
+        car = re.sub(r"en3", "ěn", car)
+        car = re.sub(r"En3", "Ěn", car)
+        car = re.sub(r"en4", "èn", car)
+        car = re.sub(r"En4", "Èn", car)
+        car = re.sub(r"eng1", "ēng", car)
+        car = re.sub(r"Eng1", "Ēng", car)
+        car = re.sub(r"eng2", "éng", car)
+        car = re.sub(r"Eng2", "Éng", car)
+        car = re.sub(r"eng3", "ěng", car)
+        car = re.sub(r"Eng3", "Ěng", car)
+        car = re.sub(r"eng4", "èng", car)
+        car = re.sub(r"Eng4", "Èng", car)
+        car = re.sub(r"in1", "īn", car)
+        car = re.sub(r"in2", "ín", car)
+        car = re.sub(r"in3", "ǐn", car)
+        car = re.sub(r"in4", "ìn", car)
+        car = re.sub(r"ing1", "īng", car)
+        car = re.sub(r"ing2", "íng", car)
+        car = re.sub(r"ing3", "ǐng", car)
+        car = re.sub(r"ing4", "ìng", car)
+        car = re.sub(r"ong1", "ōng", car)
+        car = re.sub(r"ong2", "óng", car)
+        car = re.sub(r"ong3", "ǒng", car)
+        car = re.sub(r"ong4", "òng", car)
+        car = re.sub(r"un1", "ūn", car)
+        car = re.sub(r"un2", "ún", car)
+        car = re.sub(r"un3", "ǔn", car)
+        car = re.sub(r"un4", "ùn", car)
+        car = re.sub(r"er2", "ér", car)
+        car = re.sub(r"Er2", "Ér", car)
+        car = re.sub(r"er3", "ěr", car)
+        car = re.sub(r"Er3", "Ěr", car)
+        car = re.sub(r"er4", "èr", car)
+        car = re.sub(r"Er4", "Èr", car)
+        car = re.sub(r"aō", "āo", car)
+        car = re.sub(r"Aō", "Āo", car)
+        car = re.sub(r"aó", "áo", car)
+        car = re.sub(r"Aó", "Áo", car)
+        car = re.sub(r"aǒ", "ǎo", car)
+        car = re.sub(r"Aǒ", "Ǎo", car)
+        car = re.sub(r"aò", "ào", car)
+        car = re.sub(r"Aò", "Ào", car)
+        car = re.sub(r"oū", "ōu", car)
+        car = re.sub(r"Oū", "Ōu", car)
+        car = re.sub(r"oú", "óu", car)
+        car = re.sub(r"Oú", "Óu", car)
+        car = re.sub(r"oǔ", "ǒu", car)
+        car = re.sub(r"Oǔ", "Ǒu", car)
+        car = re.sub(r"où", "òu", car)
+        car = re.sub(r"Où", "Òu", car)
+        car = re.sub(r"aī", "āi", car)
+        car = re.sub(r"Aī", "Āi", car)
+        car = re.sub(r"aí", "ái", car)
+        car = re.sub(r"Aí", "Ái", car)
+        car = re.sub(r"aǐ", "ǎi", car)
+        car = re.sub(r"Aǐ", "Ǎi", car)
+        car = re.sub(r"aì", "ài", car)
+        car = re.sub(r"Aì", "Ài", car)
+        car = re.sub(r"eī", "ēi", car)
+        car = re.sub(r"Eī", "Ēi", car)
+        car = re.sub(r"eí", "éi", car)
+        car = re.sub(r"Eí", "Éi", car)
+        car = re.sub(r"eǐ", "ěi", car)
+        car = re.sub(r"Eǐ", "Ěi", car)
+        car = re.sub(r"eì", "èi", car)
+        car = re.sub(r"Eì", "Èi", car)
+        car = re.sub(r"5", "", car)
+        return car
+
+    def clean_transcr(self, definition):
+        transcr = definition[1].strip('[').strip(']').replace(' ','')
+        transcr = self.convert_pinyin(transcr)
+        return transcr
+
+    def process_classifiers(self, definition):
+        cl = re.findall(r'/CL:.+?/',definition[2])
+        gr_cl = []
+        for c in cl:
+            gr_cl.append(c.strip('/'))
+        if len(gr_cl) == 0:
+            gr_cl = 'NO'
+        elif len(gr_cl) > 0:
+            gr_cl = ','.join(gr_cl)
+            gr_cl = gr_cl.replace('[,',',').replace('CL:','')
+            gr_cl = re.sub(r'\w\|','',gr_cl)
+            gr_cl = self.convert_pinyin(gr_cl)
+        return gr_cl
+
+    def clean_sem(self, definition):
+        print(definition)
+        defi = definition[2]
+        print(defi)
+        cls = re.findall(r'/CL:.+?/', defi)
+        for cl in cls:
+            print(cl)
+            defi = defi.replace(cl,'')
+        print(defi)
+        sem = defi.replace(' ','_').replace('/',' ').replace(',_',' ').replace(',',' ').strip('_').strip()
+        transcr = re.findall(r'\[[0-9A-z]+?\]',sem)
+        for tr in transcr:
+            tr_ = self.convert_pinyin(tr).replace('_','')
+            sem = sem.replace(tr,tr_)
+        return sem
 
     def process_para(self, txt_ru, txt_zh):
         para = ET.Element("para")
@@ -75,8 +253,8 @@ class ZhXMLProcessor():
             while txt_zh[pos:pos+cnt] in self.cedict:
                 cnt += 1
             key = txt_zh[pos:pos+cnt-1]
-            if len(key) < 1:
-                # TODO: add non-found char to hz/zh2
+            if len(key) < 1:        # not found in dict
+                # add non-found char to hz/zh2
                 key = txt_zh[pos:pos+1]
                 if last_zh is not None:
                     if last_zh.tail == None:
@@ -84,6 +262,7 @@ class ZhXMLProcessor():
                     last_zh.tail += key
                 else:
                     zh.text = zh.text + key
+                key = key.translate(self.punct_dict)
                 if last_zh2 is not None:
                     if last_zh2.tail == None:
                         last_zh2.tail = ""
@@ -93,27 +272,46 @@ class ZhXMLProcessor():
                 continue
             pos += cnt - 1
             worddef = self.cedict[key]
+            #print('KEY PRINTING...', key)
+            #print('JSON PRINTING...',self.cedict[key])
             # TODO: form correct definition
             last_zh = ET.SubElement(zh, 'w')
             last_zh2 = ET.SubElement(zh2, 'w')
             ana_zh = None
             transcr = ""
             ana_zh2 = None
+            all_transcr = []
             for definition in worddef:
+                #print(definition)
                 # Appending <ana> for each interpretation
-                transcr = definition[1]
-                desc = self.clean_definition(definition[2], key, transcr)
-                ana_zh = ET.SubElement(last_zh, "ana", lex=definition[0], transcr=transcr, sem=desc)
-                ana_zh2 = ET.SubElement(last_zh2, "ana", lex=transcr, transcr=transcr, sem=desc)
+                transcr = self.clean_transcr(definition)
+                if transcr.lower() not in all_transcr:
+                    all_transcr.append(transcr.lower())
+                desc = self.clean_sem(definition)
+                gr_cls = self.process_classifiers(definition)
+                #print(gr_cls)
+                #print(desc)
+                if desc == 'MOD' or desc == 'PFV' or desc == 'PRG' or desc == 'PST' or desc == 'EVAL' or desc == 'QUEST' or desc == 'CAUS' or desc == 'PL' or desc == 'BA' or desc == 'ATRN' or desc == 'ATRV' or desc == 'PASS' or desc == 'DIR':
+                    ana_zh = ET.SubElement(last_zh, "ana", lex=key, transcr=transcr, gr=desc)
+                    ana_zh2 = ET.SubElement(last_zh2, "ana", lex=transcr, transcr=transcr, gr=desc)
+                else:
+                    if gr_cls != 'NO':
+                        ana_zh = ET.SubElement(last_zh, "ana", lex=key, transcr=transcr, sem=desc, gr = gr_cls)
+                        ana_zh2 = ET.SubElement(last_zh2, "ana", lex=transcr, transcr=transcr, sem=desc, gr = gr_cls)
+                    else:
+                        ana_zh = ET.SubElement(last_zh, "ana", lex=key, transcr=transcr, sem=desc,gr='DEFAULT')
+                        ana_zh2 = ET.SubElement(last_zh2, "ana", lex=transcr, transcr=transcr, sem=desc,gr='DEFAULT')
             ana_zh.tail = key
-            ana_zh2.tail = transcr
+            transcr_lem = "/".join(all_transcr)
+            #print(transcr_lem)
+            ana_zh2.tail = transcr_lem
         return para
 
     def process_file(self, path):   
         # TODO: autodetect encoding from XML header/whatnot
         with open(path, 'r', encoding='utf-8') as fh:
-            new_f = open(path.rsplit('.', 1)[0] + '_processed.xml', 'wb')
-            new_f.write(b'<?xml version="1.0" encoding="utf-8"?><html>\n<head>\n</head>\n<body>')
+            new_f = open(path.rsplit('.', 1)[0] + '_processed.xml', 'w')
+            new_f.write('<?xml version="1.0" encoding="utf-8"?><html>\n<head>\n</head>\n<body>')
             html = fh.read()
             #html = html.replace('encoding="UTF-16"', '')
             root = ET.XML(html)
@@ -130,8 +328,8 @@ class ZhXMLProcessor():
                     print("Error fetching sentence!")
                 para = self.process_para(ru, zh)
                 parent[:] = para
-                new_f.write(ET.tostring(parent, pretty_print=True))
-            new_f.write(b"</body></html>")
+                new_f.write(unescape(ET.tostring(parent, pretty_print=True, method="xml").decode('utf-8')).replace(">", ">\n"))
+            new_f.write("</body></html>")
             new_f.close()
 
 if __name__ == '__main__':
