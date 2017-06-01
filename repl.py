8f4d4f53f5acfc158277ab7c2c189e655463c0e2
Maratik <marat.naiv@gmail.com>
2017-06-01 11:34:45 +0300

    Adding recent chenges in texts

--- repl.py
+++ repl.py
@@ -8,7 +8,7 @@ from lxml import objectify
 PATH = 'data'   # chinese_texts
 
 for f in os.listdir(PATH):
-    if f.endswith('8.xml') and '_processed' not in f  and 'REPL' not in f:
+    if f.endswith('mumu.xml') and '_processed' not in f  and 'REPL' not in f:
         print(f)
         with open(os.path.join(PATH, f), 'r') as fh:
             new_f = open(os.path.join(PATH, f.rsplit('.', 1)[0] + 'REPL.xml'), 'wb')
